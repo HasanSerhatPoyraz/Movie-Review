9fa18a6f646423d954f59f5d5cbab2bd993b3276
Hasan Serhat Poyraz <63045824+HasanSerhatPoyraz@users.noreply.github.com>
2021-12-24 16:30:06 +0300

    Localization bitti

---
+++ odev12/Data/Migrations/20211222095454_mgr6.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webProje.Data.Migrations
+{
+    public partial class mgr6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Reviews");
+        }
+    }
+}
---
+++ odev12/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webProje.Models
+{
+    public class Member : IdentityUser
+    {      
+       public string Name { get; set; }
+       
+
+    }
+}
