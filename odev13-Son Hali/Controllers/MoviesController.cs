2d0216638a6e160214723cd324c19973c010a39b
Hasan Serhat Poyraz <63045824+HasanSerhatPoyraz@users.noreply.github.com>
2021-12-24 18:31:28 +0300

    Api hizmeti verildi

---
+++ odev13-Son Hali/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webProje.Models;
+
+namespace webProje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/Movies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+    }   
+}
---
+++ odev13-Son Hali/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webProje.Models
+{
+    public class Review 
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int MovieId { get; set; }
+        public string HeadLine { get; set; }
+        public string Comment { get; set; }
+        public string Rating { get; set; }
+
+    }
+}
